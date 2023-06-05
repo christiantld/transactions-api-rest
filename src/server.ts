fcf1810118392f4b978393887bbe75470707199c
Christian Toledo <chrstiantoledo@gmail.com>
2023-06-04 22:22:29 -0300

    feat: add fastify cookies

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { env } from './env'
 import { transactionRoutes } from './routes/transactions'
 
 const PORT = env.PORT
 
 const app = fastify()
+app.register(cookie)
 app.register(transactionRoutes, { prefix: '/transactions' })
 app
   .listen({
