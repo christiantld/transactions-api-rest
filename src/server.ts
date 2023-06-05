30e9dd6b502f3c253da1e02d1d4fcd1a55071ffc
Christian Toledo <chrstiantoledo@gmail.com>
2023-06-04 21:28:04 -0300

    feat: register transactions plugin on server

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,11 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import crypto from 'node:crypto'
 import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
 
 const PORT = env.PORT
 
 const app = fastify()
-
-app.get('/', async (req, res) => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transaction test',
-      amount: 100,
-    })
-    .returning('*')
-
-  return transaction
-})
-
-app.get('/transactions', async (req, res) => {
-  const transactions = await knex('transactions').select('*')
-
-  return transactions
-})
-
+app.register(transactionRoutes, { prefix: '/transactions' })
 app
   .listen({
     port: PORT,
