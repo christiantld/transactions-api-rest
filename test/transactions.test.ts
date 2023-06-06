f4539148011014b7bcbe2443ee87d9eff3ae078b
Christian Toledo <chrstiantoledo@gmail.com>
2023-06-06 11:33:05 -0300

    test: test transaction by and and summary routes

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -15,6 +15,14 @@ async function getCookiesFromCreatedTransaction(): Promise<string[]> {
   return cookies
 }
 
+async function getTransactionId(cookies: string[]): Promise<string> {
+  const listTransactionsResponse = await request(app.server)
+    .get('/transactions')
+    .set('Cookie', cookies)
+  const transactionId = listTransactionsResponse.body.transactions[0].id
+  return transactionId
+}
+
 describe('Transactions routes', () => {
   beforeAll(async () => {
     await app.ready()
@@ -81,5 +89,48 @@ describe('Transactions routes', () => {
         amount: 100,
       }),
     ])
+  })
+
+  it('should be able to list a single transaction by id', async () => {
+    const cookies = await getCookiesFromCreatedTransaction()
+    const transactionId = await getTransactionId(cookies)
+    const response = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+
+    const { status, body } = response
+
+    expect(status).toBe(200)
+    expect(body.transaction).toEqual(
+      expect.objectContaining({
+        id: transactionId,
+        title: 'New transaction',
+        amount: 100,
+      }),
+    )
+  })
+
+  it('should be able to list a summary of all transactions', async () => {
+    const cookies = await getCookiesFromCreatedTransaction()
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'New debit transaction',
+        amount: 25,
+        type: 'debit',
+      })
+
+    const response = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+
+    const { status, body } = response
+
+    expect(status).toBe(200)
+    expect(body).toEqual({
+      amount: 75,
+    })
   })
 })
